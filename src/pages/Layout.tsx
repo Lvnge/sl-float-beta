521d5e5e2eb09fce4c2db1fd6eca72e037075056
Lvnge <roy9096@hotmail.com>
2025-02-18 17:05:08 -0600

    Fix main content scrolling

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -4,9 +4,12 @@ import SideBar from "../components/navigation/SideBar";
 
 const DashboardLayout = () => {
   return (
-    <div className="flex bg-zinc-300 text-zinc-600">
-      <SideBar />
-      <div className="w-full">
+    <div className="flex h-screen bg-zinc-300 text-zinc-600">
+      <aside className=" flex-shrink-0">
+        <SideBar />
+      </aside>
+
+      <div className="flex-1 overflow-y-auto p-4 bg-zinc-300">
         <Outlet /> {/* Nested routes render here */}
       </div>
     </div>
